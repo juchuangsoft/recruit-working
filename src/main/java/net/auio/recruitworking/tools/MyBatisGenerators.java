9d6c8b48d83c082dd5061f2264e693393090bc3c
“mask” <1643091610@qq.com>
2020-07-15 01:55:24 +0800

    数据库文件生成

---
+++ src/main/java/net/auio/recruitworking/tools/MyBatisGenerators.java
@@ -0,0 +1,61 @@
+package net.auio.recruitworking.tools;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┃    神兽保佑,代码无bug
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * <p>
+ * ━━━━━━感觉萌萌哒━━━━━━
+ *
+ * @Project : recruit-working
+ * @Description :  [一句话描述该类的功能]
+ * @Package Name :net.auio.recruitworking.tools
+ * @Author : 1643091610@qq.com
+ * @Blog ：https://www.cnblogs.com/xiaohaojs/
+ * @Date :2020 年 07月 13 日 1:40
+ * @ModifcationHistory : ------Who----------When---------------What----------
+ */
+public class MyBatisGenerators {
+    public static void main(String[] args){
+        try{
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            File configFile = new ClassPathResource("MyBatisGeneratorXML.xml").getFile();
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
